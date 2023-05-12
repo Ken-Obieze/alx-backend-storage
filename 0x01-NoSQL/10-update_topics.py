e66f1b91842808989f962b838c0a651cbdeb05ec
Ken-Obieze <kenneth.obieze@outlook.com>
2023-05-13 00:18:35 +0100

    task10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Updates all topics of a school document based on the school name.
+
+    Args:
+        mongo_collection: pymongo collection object
+        name: string representing the school name to update
+        topics: list of strings representing the topics to update
+
+    Returns:
+        The number of documents updated.
+    """
+    result = mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
+    return result.modified_count
+
