0a3b5d3f8b630e570bf6256d102ac51e2d2cd7a4
Ken-Obieze <kenneth.obieze@outlook.com>
2023-05-13 00:35:07 +0100

    task12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Module to return logs."""
+
+
+from pymongo import MongoClient
+
+def print_stats(collection):
+    """Print some stats about the Nginx logs stored in MongoDB"""
+    total_logs = collection.count_documents({})
+
+    print(f"{total_logs} logs")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\t{count}\t{method}")
+
+    count = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"\t{count}\tmethod=GET, path=/status")
+
+if __name__ == '__main__':
+    with MongoClient() as client:
+        db = client.logs
+        collection = db.nginx
+        print_stats(collection)
