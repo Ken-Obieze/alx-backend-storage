db7252694d5dddcebfd151b02b0c6d9f855d36f5
Ken-Obieze <kenneth.obieze@outlook.com>
2023-05-13 01:05:12 +0100

    fix

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python3
 """Module to return logs."""
-
-
 from pymongo import MongoClient
 
-def print_stats(collection):
-    """Print some stats about the Nginx logs stored in MongoDB"""
-    total_logs = collection.count_documents({})
 
-    print(f"{total_logs} logs")
-    print("Methods:")
+def print_logs_stats(collection):
+    """
+    Prints some stats about the Nginx logs stored in the given collection.
+    """
+    count = collection.count_documents({})
+    print(f"{count} logs")
 
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
     for method in methods:
         count = collection.count_documents({"method": method})
-        print(f"\t{count}\t{method}")
+        print(f"    method {method}: {count}")
 
     count = collection.count_documents({"method": "GET", "path": "/status"})
     print(f"{count} status check")
 
-if __name__ == '__main__':
-    client = MongoClient('localhost', 27017)
+
+if __name__ == "__main__":
+    client = MongoClient()
     db = client.logs
     collection = db.nginx
-    print_stats(collection)
+    print_logs_stats(collection)
+    client.close()
